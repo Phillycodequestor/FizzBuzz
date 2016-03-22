458cdf2fe28064a5d874c062a4dfefba64d66090
Matt Zager <mzager09@gmail.com>
2016-03-22 15:22:34 -0400

    new commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 $(document).ready(function(){
 
 for (var i = 1; i < 101; i++)
+{
 
-$(body).text(i);
+var value = function(number)
+{
+if (number % 3 === 0)
+{return "Fizz";}
+
+else if (number % 5 === 0)
+{return "Buzz";}
+
+else { return number;}
+
+};
+
+$('body').append(value(i));
 
 
+
+};
 });	
