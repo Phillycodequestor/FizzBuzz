20b4b6e35d77a97ccc1711514e9dc623b7eba528
Matt Zager <mzager09@gmail.com>
2016-03-23 12:19:54 -0400

    new commit

--- js/script.js
+++ js/script.js
@@ -5,7 +5,11 @@ for (var i = 1; i < 101; i++)
 
 var value = function(number)
 {
-if (number % 3 === 0)
+
+	if(number % (3*5) === 0)
+{return "FizzBuzz";}
+
+else if (number % 3 === 0)
 {return "Fizz";}
 
 else if (number % 5 === 0)
@@ -14,7 +18,7 @@ else if (number % 5 === 0)
 else { return number;}
 
 };
-
+$('body').append("<p></p>");
 $('body').append(value(i));
 
 
