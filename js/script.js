deddf51a272c7f229570f2553528e16c09974eb1
Matt Zager <mzager09@gmail.com>
2016-03-30 14:40:15 -0400

    new commit

--- js/script.js
+++ js/script.js
@@ -1,27 +1,48 @@
 $(document).ready(function(){
 
-for (var i = 1; i < 101; i++)
+
+var x = prompt("Enter an integer greater than zero.");
+
+//convert string to integer
+var integer = parseInt(x);
+
+//check if decimal
+if (integer % 1 != 0) {
+
+	alert("Please enter a whole number");
+}
+
+else if (integer < 1) {
+	alert("Please enter a number greater than zero.");
+}
+
+else {
+
+//determine if fizz, etc.
+var value = function(number) 
 {
 
-var value = function(number)
-{
-
-	if(number % (3*5) === 0)
-{return "FizzBuzz";}
+if(number % (3*5) === 0)
+	{return "FizzBuzz";}
 
 else if (number % 3 === 0)
-{return "Fizz";}
+	{return "Fizz";}
 
 else if (number % 5 === 0)
-{return "Buzz";}
+	{return "Buzz";}
 
-else { return number;}
-
+else 
+	{ return number;}
 };
+
+//call the function
+for(var i = 1; i <= integer; i++)
+
+{
 $('body').append("<p></p>");
 $('body').append(value(i));
-
-
+};
 
 };
+
 });	
